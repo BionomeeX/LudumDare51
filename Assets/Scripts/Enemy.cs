e5b988bfeaf44ce83efd2b1ba3cbda27426885a5
Xwilarg <xwilarg@protonmail.com>
2022-10-01 01:40:14 +0200

    Add enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,8 +20,12 @@ namespace LudumDare51
 
         private void FixedUpdate()
         {
-            transform.LookAt(_nextNode.transform.position, Vector3.forward);
-            _rb.velocity = transform.up * Time.fixedDeltaTime * _info.Speed;
+            var targetPos = _nextNode.transform.position;
+            targetPos.x -= transform.position.x;
+            targetPos.y -= transform.position.y;
+            var angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            _rb.velocity = _info.Speed * Time.fixedDeltaTime * transform.right;
             if (Vector2.Distance(transform.position, _nextNode.transform.position) < .1f)
             {
                 _nextNode = _nextNode.NextNode;
