17a479ee19e9d76ba0e93dc882b4b787f9e85351
Clément Carré <clement.carre@bionomeex.com>
2022-10-01 02:04:04 +0200

    Add grid gizmos

--- Assets/OnClick.cs
+++ Assets/OnClick.cs
@@ -23,6 +23,23 @@ public class OnClick : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        //Debug.Log(Screen.width);
+        //Debug.Log(Screen.height);
+
+        var x = Mathf.FloorToInt(Screen.width / _gridsize);
+        var y = Mathf.FloorToInt(Screen.height / _gridsize);
+        for(var i = 0 ; i < x; ++i){
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(new Vector2(i * _gridsize, 0), new Vector2(i * _gridsize, Screen.height));
+        }
+        for(var i = 0 ; i < y; ++i){
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(new Vector2(0, i * _gridsize), new Vector2(Screen.width, i * _gridsize));
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
