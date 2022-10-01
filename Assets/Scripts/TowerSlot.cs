8e253373a4a96a6d748d137030f9affaf4ccdba9
Xwilarg <xwilarg@protonmail.com>
2022-10-01 18:32:58 +0200

    Fix colliders

--- Assets/Scripts/TowerSlot.cs
+++ Assets/Scripts/TowerSlot.cs
@@ -1,26 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace LudumDare51
 {
     public class TowerSlot : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         private void OnMouseDown()
         {
-            OnClick.Instance.AddTower(this.transform.position);
+            OnClick.Instance.AddTower(transform.position);
         }
 
     }
