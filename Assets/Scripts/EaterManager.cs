a2f0d9dfdeffc2c96c7219e7e41860d1a8346e66
Xwilarg <xwilarg@protonmail.com>
2022-10-03 17:14:04 +0200

    Add more translations

--- Assets/Scripts/EaterManager.cs
+++ Assets/Scripts/EaterManager.cs
@@ -1,4 +1,5 @@
 ﻿using LudumDare51.Enemy;
+using LudumDare51.Translation;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -28,7 +29,7 @@ namespace LudumDare51
         private void FixedUpdate()
         {
             score += (50 - maxDiff) * Time.deltaTime;
-            _scoreText.text = $"Score: {Mathf.CeilToInt(score)}";
+            _scoreText.text = $"{Translate.Instance.Tr("score")}: {Mathf.CeilToInt(score)}";
         }
 
         private float score;
