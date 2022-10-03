da35e24985599eeaa1c832960896b8803cf83577
Xwilarg <xwilarg@protonmail.com>
2022-10-03 17:32:04 +0200

    Add tooltip text

---
+++ Assets/Scripts/ShowHelp.cs
@@ -0,0 +1,22 @@
+﻿using LudumDare51.Translation;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LudumDare51
+{
+    internal class ShowHelp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private string _helpText;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (string.IsNullOrWhiteSpace(_helpText)) return;
+            Tooltip.Instance.Show(Translate.Instance.Tr(_helpText));
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Tooltip.Instance.Hide();
+        }
+    }
+}
