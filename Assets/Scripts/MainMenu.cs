2fd3c3475ae3a047dae35350bd2a9307257aa4d9
Xwilarg <xwilarg@protonmail.com>
2022-10-03 21:46:13 +0200

    Improve french

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using LudumDare51.Translation;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace LudumDare51
@@ -8,6 +9,16 @@ namespace LudumDare51
         public void Play()
         {
             SceneManager.LoadScene("Main");
+        }
+
+        public void SetEnglish()
+        {
+            Translate.Instance.CurrentLanguage = "english";
+        }
+
+        public void SetFrench()
+        {
+            Translate.Instance.CurrentLanguage = "french";
         }
     }
 }
