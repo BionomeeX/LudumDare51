c8bb27a6151b5b4883e01a1afdab53fa2a746443
Xwilarg <xwilarg@protonmail.com>
2022-10-03 16:58:01 +0200

    Add tooptip script

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -1,8 +1,33 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace LudumDare51
 {
     public class Tooltip : MonoBehaviour
     {
+        public static Tooltip Instance { private set; get; }
+
+        [SerializeField]
+        private GameObject _container;
+
+        [SerializeField]
+        private TMP_Text _text;
+
+        private void Awake()
+        {
+            Instance = this;
+            _container.SetActive(false);
+        }
+
+        public void Show(string text)
+        {
+            _container.SetActive(true);
+            _text.text = text;
+        }
+
+        public void Hide()
+        {
+            _container.SetActive(false);
+        }
     }
 }
